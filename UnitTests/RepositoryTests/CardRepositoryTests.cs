a512ded2d3b203b8248697bd05c02b6ea2b15660
Maxim Tretyakov <scryptan@gmail.com>
2021-04-18 17:04:29 +0500

    add tests

---
+++ UnitTests/RepositoryTests/CardRepositoryTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Data;
+using Core.Models;
+using Core.Models.Dto;
+using Core.Repositories.Abstracts;
+using Core.Repositories.Realizations;
+using FluentAssertions;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+
+namespace UnitTests.RepositoryTests
+{
+    [TestFixture]
+    public class CardRepositoryTests
+    {
+        private ICardRepository _cardRepository;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+            _cardRepository = new CardRepository(new ApplicationDbContext(options, new OptionsWrapper<OperationalStoreOptions>(new OperationalStoreOptions())));
+        }
+        public static IEnumerable<TestCaseData> CreationCards
+        {
+            get
+            {
+                yield return new TestCaseData(new CreationCardDto{Answer = "some answer", Question = "text question", Type = CardType.Text});
+                yield return new TestCaseData(new CreationCardDto{Answer = "some answer", Question = "photo question", Type = CardType.Photo});
+                yield return new TestCaseData(new CreationCardDto{Answer = "some answer", Question = "mixed question", Type = CardType.Mixed});
+            }
+        }
+
+        [TestCaseSource(nameof(CreationCards))]
+        public async Task CreationCard(CreationCardDto dto)
+        {
+            var result = await _cardRepository.AddAsync(dto);
+            var found = await _cardRepository.FindAsync(result.Id);
+            
+            result.Answer.Should().BeEquivalentTo(dto.Answer);
+            result.Question.Should().BeEquivalentTo(dto.Question);
+            result.Type.Should().BeEquivalentTo(dto.Type);
+            
+            found.Should().Be(result);
+        }
+    }
+}
---
+++ UnitTests/RepositoryTests/DeckRepositoryTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Data;
+using Core.Models;
+using Core.Models.Dbo;
+using Core.Models.Dto;
+using Core.Repositories.Abstracts;
+using Core.Repositories.Realizations;
+using FluentAssertions;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+
+namespace UnitTests.RepositoryTests
+{
+    [TestFixture]
+    public class DeckRepositoryTests
+    {
+        private IDeckRepository _deckRepository;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+            _deckRepository = new DeckRepository(new ApplicationDbContext(options,
+                new OptionsWrapper<OperationalStoreOptions>(new OperationalStoreOptions())));
+        }
+
+        public static IEnumerable<TestCaseData> CreationDecks
+        {
+            get
+            {
+                yield return new TestCaseData(new CreationDeckDto {Name = "some name"});
+                yield return new TestCaseData(new CreationDeckDto
+                {
+                    Name = "some name",
+                    Cards = new List<CardDbo>
+                        {new() {Answer = "asd", Id = Guid.NewGuid(), Question = "some quest", Type = CardType.Text}}
+                });
+                yield return new TestCaseData(new CreationDeckDto
+                {
+                    Name = "some name",
+                    Cards = new List<CardDbo>
+                    {
+                        new() {Answer = "asd", Id = Guid.NewGuid(), Question = "some quest", Type = CardType.Text},
+                        new() {Answer = "asd", Id = Guid.NewGuid(), Question = "some quest", Type = CardType.Text}
+                    }
+                });
+            }
+        }
+
+        [TestCaseSource(nameof(CreationDecks))]
+        public async Task CreationCard(CreationDeckDto dto)
+        {
+            var result = await _deckRepository.AddAsync(dto);
+            var found = await _deckRepository.FindAsync(result.Id);
+
+            result.Name.Should().BeEquivalentTo(dto.Name);
+            result.Cards.Should().BeEquivalentTo(dto.Cards);
+
+            found.Should().Be(result);
+        }
+    }
+}
