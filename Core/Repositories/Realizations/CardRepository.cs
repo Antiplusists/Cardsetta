d3f4d7fe2d3b6e9f75ab543e98343af32c964a1c
Maxim Tretyakov <scryptan@gmail.com>
2021-04-18 12:30:05 +0500

    add repository realizations with NotImplementedException

---
+++ Core/Repositories/Realizations/CardRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Models.Dbo;
+using Core.Models.Dto;
+using Core.Repositories.Abstracts;
+
+namespace Core.Repositories.Realizations
+{
+    public class CardRepository: ICardRepository
+    {
+        public async Task<CardDbo?> FindAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CardDbo> AddAsync(CreationCardDto creationEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CardDbo> UpdateAsync(Guid id, CreationCardDto entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Repositories/Realizations/DeckRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Models.Dbo;
+using Core.Models.Dto;
+using Core.Repositories.Abstracts;
+
+namespace Core.Repositories.Realizations
+{
+    public class DeckRepository: IDeckRepository
+    {
+        public async Task<DeckDbo?> FindAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DeckDbo> AddAsync(CreationDeckDto creationEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DeckDbo> UpdateAsync(Guid id, CreationDeckDto entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
