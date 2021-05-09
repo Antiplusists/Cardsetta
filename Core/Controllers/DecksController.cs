6731cde3eb0275d9586791c1b72566cf98a3787f
Michael Stepanov <mihanoid10@yandex.ru>
2021-05-09 20:25:31 +0500

    Add get page by tags query

--- Core/Controllers/DecksController.cs
+++ Core/Controllers/DecksController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Core.Models;
@@ -27,12 +28,22 @@ namespace Core.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<PageListResult<DeckResult>>> GetDecks([FromQuery]int pageNumber = 1, [FromQuery]int pageSize = 10)
+        public async Task<ActionResult<PageList<DeckResult>>> GetDecksByTags([FromQuery] string[] tags,
+            [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
         {
             if (pageNumber < 1 || pageSize < 1)
-                return BadRequest();
+                return BadRequest("Page number and page size can not be less than 1");
+            if (!tags.All(tag => !string.IsNullOrEmpty(tag) && tag.All(char.IsLetterOrDigit)))
+                return BadRequest("Tags should contains only letters and digits");
+            if (!tags.All(tag => tag.All(char.IsLower)))
+                return BadRequest("Tags should be in lower case");
+            if (!tags.All(tag => tag.Length <= 30))
+                return BadRequest("Tags should not be bigger than 30 symbols");
             
-            var page = await deckRepo.GetPage(pageNumber, pageSize);
+            var page = tags.Length > 0 
+                ? await deckRepo.GetPageByTags(pageNumber, pageSize, tags) 
+                : await deckRepo.GetPage(pageNumber, pageSize);
+            
             var result = new PageListResult<DeckResult>(mapper.Map<List<DeckDbo>, List<DeckResult>>(page), page.TotalCount,
                 page.CurrentPage, page.PageSize);
             
