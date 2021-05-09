1ca3e7b62ded54bcf2a5b4c7cd1eada91154fdc7
Michael Stepanov <mihanoid10@yandex.ru>
2021-05-09 23:37:39 +0500

    Add post deck query method

--- Core/Controllers/DecksController.cs
+++ Core/Controllers/DecksController.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Core.Models;
 using Core.Models.Dbo;
+using Core.Models.Dto;
 using Core.Models.Results;
 using Core.Repositories.Abstracts;
+using IdentityServer4.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core.Controllers
@@ -18,11 +23,13 @@ namespace Core.Controllers
     {
         private readonly IDeckRepository deckRepo;
         private readonly IMapper mapper;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public DecksController(IDeckRepository deckRepo, IMapper mapper)
+        public DecksController(IDeckRepository deckRepo, IMapper mapper, UserManager<ApplicationUser> userManager)
         {
             this.deckRepo = deckRepo;
             this.mapper = mapper;
+            this.userManager = userManager;
         }
         
         [HttpGet]
@@ -48,6 +55,38 @@ namespace Core.Controllers
                 page.CurrentPage, page.PageSize);
             
             return Ok(result);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> CreateDeck([FromForm] CreationDeckDto dto)
+        {
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var user = await userManager.FindByIdAsync(User.GetSubjectId());
+
+            if (user is null)
+                return Unauthorized();
+
+            var dbo = mapper.Map(dto, new DeckDbo
+            {
+                Author = user
+            });
+
+            dbo = await deckRepo.AddAsync(dbo);
+
+            return CreatedAtRoute(nameof(GetDeckById), new {deckId = dbo.Id}, dbo.Id);
+        }
+
+        [HttpGet]
+        [Route("{deckId:guid}")]
+        public async Task<ActionResult<DeckResult>> GetDeckById([FromRoute] Guid deckId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
