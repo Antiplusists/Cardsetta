5598a2bd42ef6e63e23f95b18fc083032ca7765e
Michael Stepanov <mihanoid10@yandex.ru>
2021-05-09 19:02:21 +0500

    Initialize controller

---
+++ Core/Controllers/DecksController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Core.Models;
+using Core.Models.Dbo;
+using Core.Repositories.Abstracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Core.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    [Produces("application/json")]
+    public class DecksController : Controller
+    {
+        private readonly IDeckRepository deckRepo;
+        private readonly IMapper mapper;
+
+        public DecksController(IDeckRepository deckRepo, IMapper mapper)
+        {
+            this.deckRepo = deckRepo;
+            this.mapper = mapper;
+        }
+    }
+}
